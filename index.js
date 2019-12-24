834df461445ff87a81649869e07ec587b4a69cb6
haoyang <haoyang.menthays@bytedance.com>
2019-12-24 13:28:46 +0800

    feat: priminitively finished

--- index.js
+++ index.js
@@ -104,7 +104,4 @@ app.on('activate', async () => {
 	await app.whenReady();
 	Menu.setApplicationMenu(menu);
 	mainWindow = await createMainWindow();
-
-	const favoriteAnimal = config.get('favoriteAnimal');
-	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
 })();
--- renderer.js
+++ renderer.js
@@ -3,13 +3,25 @@ const { ipcRenderer } = require('electron')
 const MAX_SIZE = 5
 const stack =[]
 const main = document.getElementById('root')
-ipcRenderer.on('text-copied', (e, text) => {
-  console.log(text)
+
+const createItem = (text) => {
+  const newElement = document.createElement('div')
+  newElement.className = 'list-item'
+  newElement.innerText = text
+  return newElement
+}
+
+const pushToStack = (item) => {
   if (stack.size >= MAX_SIZE) {
     main.removeChild(stack[0])
     stack.pop()
   }
-  const newElement = document.createTextNode(text)
-  stack.push(newElement)
+  stack.push(item)
   main.appendChild(stack[stack.length-1])
+}
+
+ipcRenderer.on('text-copied', (e, text) => {
+  const item = createItem(text)
+  pushToStack(item)
 })
+
